e99afc668fa2742c15de0eeb87a87ad4b0357472
annalaise <annalaise.gibbons@gmail.com>
2017-07-17 15:44:09 +0100

    sinatra views complete

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'erb'
 
 get '/' do
   'hello!'
@@ -10,8 +11,5 @@ get '/secret' do
 end
 
 get '/cat' do
-  "<div>
-  <img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png' style='border:4px dashed red'>
-  </div>"
-
+  erb :index
 end
